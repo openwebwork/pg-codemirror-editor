c202afac13ee8ecb663a10793fa1fb9c0fa416ab
Glenn Rice <grice1@missouriwestern.edu>
2024-10-07 05:39:14 -0500

    Use more natural alternates for code folding on Macs.
    The CodeMirror defaults don't make sense.

--- src/pg-codemirror-editor.ts
+++ src/pg-codemirror-editor.ts
@@ -24,7 +24,17 @@ import {
     toggleLineComment,
     toggleBlockCommentByLine
 } from '@codemirror/commands';
-import { bracketMatching, foldGutter, foldKeymap, indentOnInput, indentUnit, syntaxTree } from '@codemirror/language';
+import {
+    bracketMatching,
+    foldGutter,
+    foldCode,
+    unfoldCode,
+    foldAll,
+    unfoldAll,
+    indentOnInput,
+    indentUnit,
+    syntaxTree
+} from '@codemirror/language';
 import { closeBrackets, autocompletion, closeBracketsKeymap, completionKeymap } from '@codemirror/autocomplete';
 import { highlightSelectionMatches, searchKeymap } from '@codemirror/search';
 import type { SyntaxNode } from '@lezer/common';
@@ -124,7 +134,10 @@ export class View {
             ...defaultKeymap.filter((k) => k.key !== 'Mod-Enter'),
             ...searchKeymap,
             ...historyKeymap,
-            ...foldKeymap,
+            { key: 'Ctrl-Shift-[', mac: 'Cmd-Shift-[', run: foldCode },
+            { key: 'Ctrl-Shift-]', mac: 'Cmd-Shift-]', run: unfoldCode },
+            { key: 'Ctrl-Alt-[', mac: 'Cmd-Alt-[', run: foldAll },
+            { key: 'Ctrl-Alt-]', mac: 'Cmd-Alt-]', run: unfoldAll },
             ...completionKeymap,
             indentWithTab
         ]),
